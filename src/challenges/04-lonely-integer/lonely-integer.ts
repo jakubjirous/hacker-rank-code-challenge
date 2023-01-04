b2f2f58928c3ec2b8e3a12b75e652c6d3e9cd555
Jakub Jirous <info@jakubjirous.cz>
2023-01-04 09:09:47 -0800

    4) lonely integer - forEach instead of map

--- src/challenges/04-lonely-integer/lonely-integer.ts
+++ src/challenges/04-lonely-integer/lonely-integer.ts
@@ -13,7 +13,7 @@ export type Output = number;
 export const lonelyInteger = (inputArray: Input): Output => {
   const hash = new Map<number, number>();
 
-  inputArray.map((item) =>
+  inputArray.forEach((item) =>
     hash.get(item) ? hash.delete(item) : hash.set(item, 1)
   );
 
